38b8fd75457c5b128b28dca2ac249caabbe417c2
abelzhu <abel_zhu@msn.com>
2020-02-27 01:06:57 +0000

    Deploying to gh-pages from  - 60d3d9b9d49b278b848c79a317ad46fa3db9ecba 🚀

---
+++ precache-manifest.353bf0964c98bd8bf414dde1c25d2127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1cd14a0c18f1c35f24c571086dfa72",
+    "url": "/abelsite/index.html"
+  },
+  {
+    "revision": "be2a9f6c5f550c4a4bc5",
+    "url": "/abelsite/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "342f84d2acbfa33dc270",
+    "url": "/abelsite/static/js/2.bd3446e6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/abelsite/static/js/2.bd3446e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2a9f6c5f550c4a4bc5",
+    "url": "/abelsite/static/js/main.fafba024.chunk.js"
+  },
+  {
+    "revision": "e2c479d639709f5d1dea",
+    "url": "/abelsite/static/js/runtime-main.53807c41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/abelsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
