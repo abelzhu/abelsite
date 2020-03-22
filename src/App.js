cf805957b5d3d5f90c885730f971b337cfc6cb4d
abel <abel_zhu@msn.com>
2020-03-22 19:58:09 +0800

    modify resume

--- src/App.js
+++ src/App.js
@@ -34,23 +34,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <div><h1>{helloAbel}</h1></div> */}
-        
+       
         {/* {elementWithEvent} */}
         <a href="#" onClick={clickHandle}>Click me1</a>
-        <h2>Welcome to {formatname(user)}'s page.</h2>
+        <h1>Welcome to {formatname(user)}'s page.</h>
         
+        <h2>自我介绍</h2>
+        <p>
+        热爱技术，喜欢思考，爱读书；15年以上的工作经验，12年在欧美外企，英语流利；有11年以上的研发部主管/项目主管/架构经验；7年技术总监经验；熟悉软件开发生命周期，各种设计模式，构建云服务，带队实现过多个中等规模的软件平台；多次开发团队的组建和培训经验；涉及的技术面比较广；研发团队管理经验丰富；
+        </p>
         {/* {getGreeting(user)}
         {getGreeting()} */}
         
